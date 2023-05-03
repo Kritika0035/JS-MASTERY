5cc2f79d6ee9d7fd4af17c7e499454218c95a9d6
Kritika gupta <92821377+Kritika0035@users.noreply.github.com>
2023-05-04 00:43:33 +0530

    jh

--- file1.js
+++ file1.js
@@ -1,3 +1,8 @@
 console.log ("helloo world"); //console.log can print anything.
 
+// introduction to variable
+// variables are used to store information and it can be used again whenever its needed
 
+var firstname = "kritika"; //firstname declared as variable.
+
+console.log(firstname); //the firstname used as variable to print output.
