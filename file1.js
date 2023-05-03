38108e5efa2ca4f2e945a9f47c7f0a93b9da1ad6
Kritika gupta <92821377+Kritika0035@users.noreply.github.com>
2023-05-04 01:28:56 +0530

    Var, Let, output

--- file1.js
+++ file1.js
@@ -1,8 +1,26 @@
 console.log ("helloo world"); //console.log can print anything.
 
+"use strict"; //to follow rules of js
+
 // introduction to variable
 // variables are used to store information and it can be used again whenever its needed
+// var cannot start with a number, but can start with a _, or $.
+// can also use camelcase
 
 var firstname = "kritika"; //firstname declared as variable.
 
 console.log(firstname); //the firstname used as variable to print output.
+
+var value =5;
+console.log(value**2);
+
+let firstName = "harshit" // "let" save you from giving the same name to different values.
+console.log(firstName + " is a good guy");
+
+// contstant introduction
+// constant value cant be changed through out the code
+
+const pi=3.14;
+let r = 5;
+console.log(pi*r*r); //can do operations of constant value.
+
