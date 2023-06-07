ec9730422d84762672d5e9310a51f41d9661e17e
Kritika gupta <92821377+Kritika0035@users.noreply.github.com>
2023-06-07 23:54:34 +0530

    Conditional statements

--- file4.js
+++ file4.js
@@ -9,3 +9,27 @@ else
 {
     console.log("can play mario"); //true
 }
+
+let winnum = 19;
+let guess = +prompt("guess the number"); // to take input
+
+console.log(typeof guess, guess);
+
+//switch statements
+
+let day=2;
+
+switch(day){
+    case 0:
+        console.log("sunday");
+        break; // so that it terminates then n there
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    default:
+        console.log("invalid day");
+        break;
+}
